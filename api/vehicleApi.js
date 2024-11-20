9d0fdb3e0cbf97db1c92467cffd7c2e0cbcad448
Amodhx <amxdhnanditha@gmail.com>
2024-11-21 01:08:07 +0530

    handled vehicle api calling error alerts

--- api/vehicleApi.js
+++ api/vehicleApi.js
@@ -27,9 +27,26 @@ export default class VehicleApi{
                     resolve();
                 },
                 error: function(xhr, status, error) {
-                    console.error("Error: " + error);
-                    Swal.fire("Data Fetch Error");
-                    console.error("Response:", xhr.responseText);
+                    switch (xhr.status) {
+                        case 400:
+                            Swal.fire("Bad Request", "The request was invalid. Please check your input and try again.", "error");
+                            break;
+                        case 401:
+                            Swal.fire("Unauthorized", "You are not authorized to perform this action.", "warning");
+                            break;
+                        case 403:
+                            Swal.fire("Forbidden", "You do not have permission to access this resource.", "error");
+                            break;
+                        case 404:
+                            Swal.fire("Not Found", "The requested resource could not be found.", "info");
+                            break;
+                        case 500:
+                            Swal.fire("Server Error", "An error occurred on the server. Please try again later.", "error");
+                            break;
+                        default:
+                            Swal.fire("Error", "An unexpected error occurred. Please try again.", "error");
+                            break;
+                    }
                     reject(error);
                 }
             });
@@ -59,9 +76,26 @@ export default class VehicleApi{
                     resolve();
                 },
                 error: function(xhr, status, error) {
-                    console.error("Error: " + error);
-                    Swal.fire("Data Fetch Error");
-                    console.error("Response:", xhr.responseText);
+                    switch (xhr.status) {
+                        case 400:
+                            Swal.fire("Bad Request", "The request was invalid. Please check your input and try again.", "error");
+                            break;
+                        case 401:
+                            Swal.fire("Unauthorized", "You are not authorized to perform this action.", "warning");
+                            break;
+                        case 403:
+                            Swal.fire("Forbidden", "You do not have permission to access this resource.", "error");
+                            break;
+                        case 404:
+                            Swal.fire("Not Found", "The requested resource could not be found.", "info");
+                            break;
+                        case 500:
+                            Swal.fire("Server Error", "An error occurred on the server. Please try again later.", "error");
+                            break;
+                        default:
+                            Swal.fire("Error", "An unexpected error occurred. Please try again.", "error");
+                            break;
+                    }
                     reject(error);
                 }
             });
@@ -104,10 +138,26 @@ export default class VehicleApi{
                     resolve();
                 },
                 error: function(xhr, status, error) {
-                    console.error("Error: " + error);
-
-                    Swal.fire("Data Fetch Error");
-                    console.error("Response:", xhr.responseText);
+                    switch (xhr.status) {
+                        case 400:
+                            Swal.fire("Bad Request", "The request was invalid. Please check your input and try again.", "error");
+                            break;
+                        case 401:
+                            Swal.fire("Unauthorized", "You are not authorized to perform this action.", "warning");
+                            break;
+                        case 403:
+                            Swal.fire("Forbidden", "You do not have permission to access this resource.", "error");
+                            break;
+                        case 404:
+                            Swal.fire("Not Found", "The requested resource could not be found.", "info");
+                            break;
+                        case 500:
+                            Swal.fire("Server Error", "An error occurred on the server. Please try again later.", "error");
+                            break;
+                        default:
+                            Swal.fire("Error", "An unexpected error occurred. Please try again.", "error");
+                            break;
+                    }
                     reject(error);
                 }
             });
