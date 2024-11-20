eaabef0b5f6823397b88d02c6c1b2dc18f3da7bc
Amodhx <amxdhnanditha@gmail.com>
2024-11-20 14:29:58 +0530

    handled crop api calling error alerts

--- api/cropApi.js
+++ api/cropApi.js
@@ -46,9 +46,26 @@ export default class CropApi{
                     resolve();
                 },
                 error: function(xhr, status, error) {
-                    console.error("Error: " + error);
-                    Swal.fire("Data Fetch Error");
-                    console.error("Response:", xhr.responseText);
+                    switch (xhr.status) {
+                        case 400:
+                            Swal.fire("Bad Request", "The request was invalid. Please check your input and try again.", "error");
+                            break;
+                        case 401:
+                            Swal.fire("Unauthorized", "You are not authorized to perform this action.", "warning");
+                            break;
+                        case 403:
+                            Swal.fire("Forbidden", "You do not have permission to access this resource.", "error");
+                            break;
+                        case 404:
+                            Swal.fire("Not Found", "The requested resource could not be found.", "info");
+                            break;
+                        case 500:
+                            Swal.fire("Server Error", "An error occurred on the server. Please try again later.", "error");
+                            break;
+                        default:
+                            Swal.fire("Error", "An unexpected error occurred. Please try again.", "error");
+                            break;
+                    }
                     reject(error);
                 }
             });
@@ -104,37 +121,27 @@ export default class CropApi{
                     resolve();
                 },
                 error: async function (xhr, status, error) {
-
-                    if (xhr.status === 401 || xhr.status === 403) {
-                        try {
-                            const newToken = await user_api.refreshToken();
-                            if (newToken) {
-                                makeRequest(newToken);
-                            } else {
-                                Swal.fire("Unauthorized", "Please login again.", "warning");
-                                reject("Token refresh failed");
-                            }
-                        } catch (err) {
-                            Swal.fire("Error", "Unable to refresh token. Please login again.", "error");
-                            reject(err);
-                        }
-                    } else {
-                        switch (xhr.status) {
-                            case 400:
-                                Swal.fire("Bad Request", "The request was invalid. Please check your input and try again.", "error");
-                                break;
-                            case 404:
-                                Swal.fire("Not Found", "The requested resource could not be found.", "info");
-                                break;
-                            case 500:
-                                Swal.fire("Server Error", "An error occurred on the server. Please try again later.", "error");
-                                break;
-                            default:
-                                Swal.fire("Error", "An unexpected error occurred. Please try again.", "error");
-                                break;
-                        }
-                        reject(xhr.responseText);
+                    switch (xhr.status) {
+                        case 400:
+                            Swal.fire("Bad Request", "The request was invalid. Please check your input and try again.", "error");
+                            break;
+                        case 401:
+                            Swal.fire("Unauthorized", "You are not authorized to perform this action.", "warning");
+                            break;
+                        case 403:
+                            Swal.fire("Forbidden", "You do not have permission to access this resource.", "error");
+                            break;
+                        case 404:
+                            Swal.fire("Not Found", "The requested resource could not be found.", "info");
+                            break;
+                        case 500:
+                            Swal.fire("Server Error", "An error occurred on the server. Please try again later.", "error");
+                            break;
+                        default:
+                            Swal.fire("Error", "An unexpected error occurred. Please try again.", "error");
+                            break;
                     }
+                        reject(error);
                 }
             });
         });
@@ -179,10 +186,26 @@ export default class CropApi{
                     resolve();
                 },
                 error: function(xhr, status, error) {
-                    console.error("Error: " + error);
-
-                    Swal.fire("Data Fetch Error");
-                    console.error("Response:", xhr.responseText);
+                    switch (xhr.status) {
+                        case 400:
+                            Swal.fire("Bad Request", "The request was invalid. Please check your input and try again.", "error");
+                            break;
+                        case 401:
+                            Swal.fire("Unauthorized", "You are not authorized to perform this action.", "warning");
+                            break;
+                        case 403:
+                            Swal.fire("Forbidden", "You do not have permission to access this resource.", "error");
+                            break;
+                        case 404:
+                            Swal.fire("Not Found", "The requested resource could not be found.", "info");
+                            break;
+                        case 500:
+                            Swal.fire("Server Error", "An error occurred on the server. Please try again later.", "error");
+                            break;
+                        default:
+                            Swal.fire("Error", "An unexpected error occurred. Please try again.", "error");
+                            break;
+                    }
                     reject(error);
                 }
             });
