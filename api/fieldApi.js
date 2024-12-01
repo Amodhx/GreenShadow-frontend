1a6202387c5d3168b47f00dce15fc1cda1f7c2e9
Amodhx <amxdhnanditha@gmail.com>
2024-12-01 15:50:39 +0530

    created auto logout when token expired

--- api/fieldApi.js
+++ api/fieldApi.js
@@ -23,10 +23,29 @@ export default class FieldApi {
                     resolve();
                 },
                 error: function(xhr, status, error) {
-                    console.error("Error: " + error);
-
-                    Swal.fire("Data Fetch Error");
-                    console.error("Response:", xhr.responseText);
+                    switch (xhr.status) {
+                        case 400:
+                            Swal.fire("Bad Request", "The request was invalid. Please check your input and try again.", "error");
+                            break;
+                        case 401:
+                            Swal.fire("Unauthorized", "You are not authorized to perform this action.", "warning");
+                            break;
+                        case 403:
+                            Swal.fire("Forbidden", "You do not have permission to access this resource.", "error");
+                            window.location.replace('index.html');
+                            localStorage.removeItem("securityKey")
+                            localStorage.removeItem("jwtToken")
+                            break;
+                        case 404:
+                            Swal.fire("Not Found", "The requested resource could not be found.", "info");
+                            break;
+                        case 500:
+                            Swal.fire("Server Error", "An error occurred on the server. Please try again later.", "error");
+                            break;
+                        default:
+                            Swal.fire("Error", "An unexpected error occurred. Please try again.", "error");
+                            break;
+                    }
                     reject(error);
                 }
             });
